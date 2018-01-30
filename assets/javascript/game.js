7f48bc8f537e8026cc1f48ea05e938ac0aa8ddba
Steven Bender <sbender1990@gmail.com>
2018-01-29 23:30:12 -0600

    started listing bands

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,4 @@
-var wordList = ["weezer","nirvina", "social distortion"]
+var wordList = ["weezer","nirvina", "social distortion", "cake",
+                 "gold finger", "rage against the machine",
+                  "red hot chili peppers", "blink 182",
+                   "soundgarden", "soul asylum"];
