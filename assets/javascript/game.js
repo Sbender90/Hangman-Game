19d4979c3fe1b3a7432e6fae691b3d96bb06b8d2
Steven Bender <sbender1990@gmail.com>
2018-01-30 01:09:19 -0600

    able to generate a word but cannot display it hidden in the browser for user to start guessing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,17 @@
-var wordList = ["weezer","nirvina", "social distortion", "cake",
-                 "gold finger", "rage against the machine",
-                  "red hot chili peppers", "blink 182",
-                   "soundgarden", "soul asylum"];
+var wordList = ["weezer", "nirvina", "social distortion", "cake",
+    "gold finger", "rage against the machine",
+    "red hot chili peppers", "blink 182",
+    "soundgarden", "soul asylum"
+];
+
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+console.log(randomWord);
+
+// document.getElementById("randomWord").textContent = randomWord;
+
+var start = alert('Press a letter to start!');
+
+if(start == randomWord) {
+
+}
