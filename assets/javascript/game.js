caeb74729a35b6472b8ea3f24649715bee7cbb12
Steven Bender <sbender1990@gmail.com>
2018-02-03 21:59:16 -0600

    c effort i hope

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var wordList = ["weezer", "nirvina", "social distortion", "cake",
-    "gold finger", "rage against the machine",
+    "goldfinger", "rage against the machine",
     "red hot chili peppers", "blink 182",
     "soundgarden", "soul asylum"
 ];
